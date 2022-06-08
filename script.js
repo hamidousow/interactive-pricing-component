48a83be18ddd00299d679f97c5460b58ab1dc892
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-06-08 15:25:27 +0200

    clean

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const slider = document.getElementById("slider");
 
-const numberPagesViews = document.getElementById('pageViews');
 let price1 = document.getElementById('price');
 let price2 = document.getElementById('price2');
 let pageViews = document.getElementById('pageViews');
 let checkbox = document.getElementById('checkbox');
-
-
-// pageViews.innerHTML = nbrPages;
 
 let yearlyPrice = ["5.25" ,"9.00" ,"12.00", "18.00", "24.00"];
 let monthlyPrice = ["7.00" ,"12.00" ,"16.00", "24.00", "32.00"];
@@ -32,50 +28,38 @@ checkbox.addEventListener('click', ()=> {
 function activeBilling() {
     if(yearlyBilling == false) {
         yearlyBilling = true;
-        console.log("prix reduit")
+        // console.log("prix reduit")
     
     } else {            
         yearlyBilling = false;
-        console.log("prix normal")                        
+        // console.log("prix normal")                        
     }
 }
 
 function setPrice() {
     price = yearlyBilling ? yearlyPrice : monthlyPrice;    
     let i = slider.value;
-    // price = monthlyPrice[i]
     price1.innerHTML = price[i];
     price2.innerHTML = price[i];      
-
-    // while(yearlyBilling) {
-    //     price1.innerHTML = parseInt(price);
-    //     break
-    // }
 }
 
 function setPageViews() {
     while(true) {
         if(slider.value == 0) {
             pageViews.innerHTML = "100k";
-            // price1.innerHTML = "7";
         }
         else if(slider.value == 1) {
             pageViews.innerHTML = "200k";
-            // price1.innerHTML = "12";
         }
         else if(slider.value == 2) {
             pageViews.innerHTML = "400k";
-            // price1.innerHTML = "16";
         }
         else if(slider.value == 3) {
             pageViews.innerHTML = "800k";
-            // price1.innerHTML = "24";
         }
         else if(slider.value == 4) {
             pageViews.innerHTML = "1600k";
-            // price1.innerHTML = "32";
         }
-
         break;
     }
 }
